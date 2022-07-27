3a17c4ee9a2b884ab7e0a2b450751698c14665a7
Christoffer Andersen <christoffer.andersen@oakdigital.dk>
2022-07-27 16:15:18 +0200

    building for static hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/styles/style.scss"],
-  components: {
-    global: true,
-    dirs: ["~/components"],
-  },
+    css: ['@/assets/styles/style.scss'],
+    ssr: false,
+    // components: {
+    //   global: true,
+    //   dirs: ["~/components"],
+    // },
 });
